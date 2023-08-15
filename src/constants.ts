c08db9116c56c5ac3e1346dd60275ab1bdb37e31
Snow-Hash-Code <gerardoluis206@gmail.com>
2023-08-14 20:12:50 -0600

    Terminado!

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const SUPPORTED_LANGUAGES = {
+  en: 'English',
+  es: 'Español',
+  nah: 'Náhuatl',
+  de: 'Deutsch'
+}
+
+export const VOICE_FOR_LANGUAGE = {
+  en: 'en-GB',
+  es: 'es-MX',
+  nah: 'es-MX',
+  de: 'de-DE'
+}
+
+export const AUTO_LANGUAGE = 'auto'
